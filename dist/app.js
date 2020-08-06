94ad10b275c82b5ebb6089d97c8b32e7250a5133
kftdev001 <kftdev001@gmail.com>
2020-08-06 13:57:08 +0900

    section72  クラスでのインタフェースの実装

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,17 @@
 "use strict";
-let user1;
-user1 = {
-    name: 'Max',
-    age: 30,
-    greet(phrase) {
-        console.log(phrase + ' ' + this.name);
+class Person {
+    constructor(n) {
+        this.age = 30;
+        this.name = n;
     }
-};
-user1.greet('Hello I am');
+    greet(phrase) {
+        console.log(phrase + " " + this.name);
+    }
+}
+// let user1: Greetable;
+let user1;
+user1 = new Person('Max');
+// PersonクラスはGreetableのインターフェースを実装しているのでuser1に設定可能
+user1.greet("Hello I am");
+console.log(user1);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,41 @@
-interface Person {
+interface Greetable {
+  // type Person は実質的にinterface Personと同じ。
+  // typeはカスタム型
   // javascript には interface は存在しない。
+  // interface と custom 型は同じように使うことができる。
+  // interface は object の型だけを定義できる。typeはunionなども定義できる。
+
+  /*
+  intefaceによって特定の構造を持つことを強制できる。
+  greetメソッドをuser1を持つことがわかる。user1がPersonであるか他のクラスのobjectかは気にせず、
+  greetメソッドを持つことが担保できる。
+  public/privateは指定できない。
+  readonlyは指定できる。
+  */
   name: string;
-  age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+  // クラスは1つしか継承できないが、interfaceは複数実装(implements)できる。
+  name: string;
+  age = 30;
+  constructor(n: string) {
+    this.name = n;
+  }
 
-user1 = {
-  name: 'Max',
-  age: 30,
-  greet(phrase: string){
-    console.log(phrase + ' ' + this.name);
+  greet(phrase: string) {
+    console.log(phrase + " " + this.name);
   }
 }
 
-user1.greet('Hello I am');
+// let user1: Greetable;
+let user1: Greetable;
+
+user1 = new Person('Max');
+// PersonクラスはGreetableのインターフェースを実装しているのでuser1に設定可能
+
+user1.greet("Hello I am");
+console.log(user1)
+
